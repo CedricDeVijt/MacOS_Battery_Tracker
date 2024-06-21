501264b80a205f56b36d246fc9d371321762d931
CedricDeVijt <cedric.devijt@gmail.com>
2024-06-21 15:37:40 +0200

    updated README.md

--- battery_tracker.sh
+++ battery_tracker.sh
@@ -1,6 +1,7 @@
-cd src
+#!/bin/zsh
+cd src || exit
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt > /dev/null
 python battery_tracker.py
 deactivate
